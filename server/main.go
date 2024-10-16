3e2c7b64e592f35b707e4eec680d9640bf37fab0
Gabriel Prostitis <prostitisgabriel@gmail.com>
2024-10-17 00:30:27 +0200

    added command line argument support when running the server

--- server/main.go
+++ server/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"hatchat/server/server"
+	"os"
+	"strconv"
 )
 
 func main() {
+	if len(os.Args) != 3 {
+		panic("bad args")
+	}
+
+	address := os.Args[1]
+	port, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		panic("bad port")
+	}
+
 	server := new(server.Server)
-	server.Run(":8080", 2)
+	server.Run(fmt.Sprintf("%s:%d", address, port), 2)
 }
